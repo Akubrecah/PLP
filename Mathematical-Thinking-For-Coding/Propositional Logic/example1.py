aecfdb43a88b4dfbbd19e77487c6e4306e40f70e
Akubrecah <poweldayck@gmail.com>
2025-07-27 02:31:42 +0300

    .

---
+++ Mathematical-Thinking-For-Coding/Propositional Logic/example1.py
@@ -0,0 +1,7 @@
+A = True
+B = False
+
+# print(type("A and B:", A and B))
+print(type("A or B:", A or B))
+print(type("not A:", not A))
+print(type("A**B:", A ** B))  # This is equivalent to A and B in boolean context
