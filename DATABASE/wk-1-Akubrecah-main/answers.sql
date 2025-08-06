7cf26dcf6dda33a95ee634c8ccfef2b66ee68e6b
Akubrecah <poweldayck@gmail.com>
2025-08-06 20:18:21 +0300

    inserted comments

--- DATABASE/wk-1-Akubrecah-main/answers.sql
+++ DATABASE/wk-1-Akubrecah-main/answers.sql
@@ -1,6 +1,7 @@
-
+-- Write an SQL query to create a new database called "salesDB"
 CREATE DATABASE IF NOT EXISTS salesDB
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
+-- SQL query to drop (delete) the database called "Demo"
 DROP DATABASE IF EXISTS demo;
