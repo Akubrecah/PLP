9e91113a7880dbf15b08218408069fa30ae30ee2
Akubrecah <poweldayck@gmail.com>
2025-08-12 21:54:33 +0300

    intrduced elif staements

--- Python/WEEK 3/conditional_statements/if.py
+++ Python/WEEK 3/conditional_statements/if.py
@@ -1,5 +1,10 @@
-temperature = 20
-if temperature > 20:
+temperature = 36
+
+if temperature > 25:
     print("Its a hot Day.")
+
+elif temperature > 35:
+    print("Its a hot day.")
+
 else:
     print("Its a cold day stay warm!")
